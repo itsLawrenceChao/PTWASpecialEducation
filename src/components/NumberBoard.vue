8db28cb57a60dc4a57680fef6cc09c957eab6996
2min0623 <blackmaple.hsiao@gmail.com>
2025-03-07 10:16:15 +0800

    feat: numberboard新增輸入功能

--- src/components/NumberBoard.vue
+++ src/components/NumberBoard.vue
@@ -1,17 +1,22 @@
 <template>
-  <!-- Your component's HTML template goes here -->
-  <table class="number-board-container">
+  <table class="NumberBoard">
     <tr class="EachRow">
       <td v-for="unit in Unit" class="UnitEachBlanket">{{ unit }}</td>
     </tr>
     <tr class="EachRow">
-      <td v-for="number in numbers" class="EachBlanket">{{ number }}</td>
+      <td
+        v-for="(number, index) in numbers"
+        class="EachBlanket"
+        :class="{ clickable: isInput }"
+        @click="isInput ? incrementDigit(index) : null"
+      >
+        {{ number }}
+      </td>
     </tr>
   </table>
 </template>
 
 <script>
-import { set } from "@vueuse/core";
 export default {
   name: "NumberBoard",
   props: {
@@ -20,18 +25,17 @@ export default {
       required: true,
     },
   },
+  emits: ["replyAnswer"],
   data() {
     return {
       numbers: [],
       Unit: null,
-      putOnNumberArray: [],
+      isInput: this.Data.isInput || false,
     };
   },
   created() {
-    // 如果單位和數字的長度不一樣，則補齊。
     this.Unit = this.Data.Unit;
     this.numbers = this.Data.Number;
-    console.log(this.Data.Number);
     if (typeof this.Data.Number === "number") {
       this.numbers = this.Data.Number.toString().split("");
     }
@@ -48,31 +52,85 @@ export default {
         }
       }
     }
-    console.log(this.numbers);
+
+    if (this.isInput) {
+      for (var i in this.numbers) {
+        this.numbers[i] = "0";
+      }
+    }
+  },
+  methods: {
+    incrementDigit(index) {
+      if (!this.isInput) return;
+
+      this.numbers[index] = (
+        (parseInt(this.numbers[index]) + 1) %
+        10
+      ).toString();
+      this.checkAnswer();
+    },
+    checkAnswer() {
+      if (!this.isInput) return;
+
+      let check = true;
+      const correctAnswer = this.Data.Number.toString().split("");
+      for (var i = correctAnswer.length - 1; i >= 0; i--) {
+        if (correctAnswer[i] !== this.numbers[i]) {
+          check = false;
+          break;
+        }
+      }
+      this.$emit("replyAnswer", check);
+    },
   },
 };
 </script>
-<style scoped lang="scss">
-/* Your component's CSS styles go here */
 
-.number-board-container {
+<style scoped lang="scss">
+.NumberBoard {
+  height: auto;
   border: solid;
   border-color: blue;
-  display: grid;
-  grid-template-rows: 1.5fr 1fr;
+  display: inline-block; // 使用 inline-block 讓表格根據內容自動調整大小
+  max-width: fit-content;
+  margin: 0 auto;
 }
+
 .EachRow {
   width: 100%;
+  height: auto;
   display: flex;
   justify-content: space-around;
-  height: 100%;
+  font-family: "YuanQuan";
+
   td {
     display: flex;
     justify-content: center;
     align-items: center;
     width: 100%;
-    height: 100%;
     border: solid;
+  }
+}
+
+.UnitEachBlanket {
+  font-size: 1.5em;
+  text-align: center;
+  writing-mode: vertical-rl; /* 直排，從右到左 */
+  text-orientation: upright; /* 保持字體直立 */
+  letter-spacing: 0.2em;
+}
+
+.EachBlanket {
+  font-size: 1.5em; // 增大字體大小
+  text-align: center;
+
+  &.clickable {
+    cursor: pointer;
+    user-select: none;
+
+    &:hover {
+      background-color: #f0f0f0;
+    }
   }
 }
 </style>
