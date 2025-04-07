aaa5d17c11a10da02ad572cec9f6bcc68b4f8e7b
2min0623 <blackmaple.hsiao@gmail.com>
2025-04-07 14:55:04 +0800

    chore: 更新版權年份至2025年

--- src/views/index.vue
+++ src/views/index.vue
@@ -16,7 +16,7 @@
     </div>
     <footer class="" style="background-color: #f19c79">
       <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-        © 2024 版權所有:
+        © 2025 版權所有:
         <a class="text-white" href="https://programtheworld.tw/">
           programtheworld.tw
         </a>
