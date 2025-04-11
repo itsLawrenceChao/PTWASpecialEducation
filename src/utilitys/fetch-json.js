863f10b06aad7f35daf02194979661f56ba0ab3c
2min0623 <blackmaple.hsiao@gmail.com>
2025-04-11 09:04:47 +0800

    feat: 改進 fetch-json 的錯誤處理機制

--- src/utilitys/fetch-json.js
+++ src/utilitys/fetch-json.js
@@ -1,11 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 const tool = axios.create({
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 
-export default async function(url) {
-  const res = await tool.get(url);
-  return res;
+export default async function (url) {
+  try {
+    const res = await tool.get(url);
+    console.log("we");
+    return res;
+  } catch (error) {
+    console.log("error");
+    if (error.response && error.response.status === 404) {
+      // 檔案不存在
+      throw new Error(`找不到檔案：${url}`);
+    }
+    // 其他錯誤
+    throw error;
+  }
 }
